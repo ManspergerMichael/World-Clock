1c6e50391e1cff3bc384c78734262dc87e05767e
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-14 13:51:06 -0700

    Got the script working
    The trick was to set the script to run after the DOM loads

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("JS is working!");
+window.onload = 
+
+function init(){
+    var time = new Date();
+    var PST = document.getElementById("PST");
+    var EST = document.getElementById("EST");
+    PST.innerHTML = time.toTimeString();
+    EST.innerHTML = time.toTimeString();
+    
+}
