3e811d47b5be85d3e8863f45321ca6debcc11804
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-14 14:32:21 -0700

    Added some stuff
    Get a single time, GMT, and calculate PST and EST

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//this event waits until the DOM is finished loading
+//this allows the script to find the elements specified
 window.onload = 
 
 function init(){
     var time = new Date();
+    //var TacomaTime = time.setHours(time.getHours - 3);
     var PST = document.getElementById("PST");
     var EST = document.getElementById("EST");
-    PST.innerHTML = time.toTimeString();
+    PST.innerHTML = time.setHours(21);
     EST.innerHTML = time.toTimeString();
     
 }
