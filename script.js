7706426cf51e6b2c5b0069e19f0497329571f9f6
Michael Mansperger <ManspergerMichael@gmail.com>
2019-03-14 17:57:53 -0700

    Version 1.0 complete
    To Do: Include weather API

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ window.onload =
 
 function init(){
     var time = new Date();
-    console.log(time.getHours().toString() + ":"+time.getMinutes().toString());
     var PST = document.getElementById("PST");
     var EST = document.getElementById("EST");
-    PST.innerHTML = ""+((time.getHours()- 12)-3)+":"+time.getMinutes();
-    EST.innerHTML = ""+(time.getHours()- 12)+":"+time.getMinutes();
+
+    //12 is subtracted to convert from 24 hour clock to 12 hour
+    if(time.getHours() > 12)
+    {
+        //system time is used, because M is in the eastrn time zone 
+        //EST will use system time and PST will display system time - 3 hours
+        
+        PST.innerHTML = ""+((time.getHours()- 12)-3)+":"+time.getMinutes()+" PM";
+        EST.innerHTML = ""+(time.getHours()- 12)+":"+time.getMinutes()+" PM";
+    }
+    else
+    {
+        PST.innerHTML = ""+time.getHours()-3+":"+time.getMinutes()+" AM";
+        EST.innerHTML = ""+time.getHours()+":"+time.getMinutes()+" AM";
+    }
+    
     
 }
