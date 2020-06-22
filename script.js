c37df8a93e1e1732132e4fd60ac8e0e3d50034e7
Michael Mansperger <ManspergerMichael@gmail.com>
2020-06-22 14:05:39 -0700

    World clock v 1.5. Worldclock.org api. Using unixtime

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
-//this event waits until the DOM is finished loading
-//this allows the script to find the elements specified
-window.onload = 
+$(document).ready(function(){
+    /* 
+    -- First attempt with Google TimeZone API
+    //Richmond corrdanates
+    var location = "47.260653, -122.493906";
+    //'37.740226, -84.2897087';
+    //create new Date object
+    var Target_Date = new Date();
+    //get timecode
+    var Target_Time = Target_Date.getTime()/1000 + Target_Date.getTimezoneOffset() * 60;
 
-function init(){
-    //Date object is generated from system time, and is where all the data comes from
-    var time = new Date();
-    var PST = document.getElementById("PST");
-    var EST = document.getElementById("EST");
-    //these variables are used to build the strings that will display the time
-    var pstHours;
-    var pstMinutes;
-    var estHours;
-    var estMinutes;
-    var timeThing;
-    var minutesPlace;
+    var API_KEY = '';
 
-    //12 is subtracted to convert from 24 hour clock to 12 hour
-    if(time.getHours() > 12)
-    {
-        //subtracting 15 to include time diffrence
-        pstHours = time.getHours()- 15;
-        estHours = time.getHours() - 12;
-        timeThing = "PM";
-    }
-    else{
-        //subtracting 3 for time diffrence
-        pstHours = time.getHours()-3;
-        estHours = time.getHours();
-        timeThing = "AM";
-    }
-    //get minutes from time object
-    pstMinutes = time.getMinutes();
-    estMinutes = time.getMinutes();
+    //create API URL 
+    var GAPI_Call =
+    'https://maps.googleapis.com/maps/api/timezone/json?location=' + location + '&timestamp='+ Target_Time + '&key='+ API_KEY; 
+    */
+
+    //Free world clock API
+    var Pacific_Time = 'http://worldtimeapi.org/api/timezone/America/Los_Angeles';
     
-    //if minutes is under 10 only one digit will display
-    if(time.getMinutes() < 10)
-    {
-        minutesPlace = ":0";
-    }
-    else
-    {
-        minutesPlace = ":";
-    }
-    //build string and set to DOM
-    PST.innerHTML = "" + pstHours + minutesPlace + pstMinutes + " " + timeThing;
-    EST.innerHTML = "" + estHours + minutesPlace + estMinutes + " " + timeThing;
+    var East_time = 'http://worldtimeapi.org/api/timezone/AMERICA/KENTUCKY/LOUISVILLE';
 
-    //I'm hilarious!
-    if(pstHours === 4 && pstMinutes === 20)
-    {
-        this.document.getElementById("blazeIt").innerHTML = "...Dave's not here man...";
-    }
-    console.log('http://api.timezonedb.com/v2.1/list-time-zone')
-}
+    $.get(Pacific_Time, function(West_data){
+        console.log(West_data);
+        var unixtime = West_data.unixtime;
+        console.log(unixtime);
+        var date = new Date(unixtime * 1000);
+        var AM_PM = 'AM';
+        var hours = date.getHours();
+        if(hours >= 12){
+            hours = hours - 12;
+            AM_PM = 'PM';
+        }
+
+        var PST = "" + hours + ":"+date.getMinutes() + " " + AM_PM;
+        $('#PST').text(PST);
+    });
+
+    $.get(East_time, function(East_data){
+        console.log(East_data);
+        var unixtime = East_data.unixtime;
+        console.log(unixtime);
+        var date = new Date(unixtime * 1000);
+        var hours = date.getHours();
+        if(hours >= 12){
+            hours = hours - 12;
+            AM_PM = 'PM';
+        }
+
+        var EST = "" + hours + ":"+date.getMinutes() + " " + AM_PM;
+        $('#EST').text(EST);
+    });
+    
+
+});
+
